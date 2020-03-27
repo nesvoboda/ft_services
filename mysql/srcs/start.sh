36296fdb4f3ac295ed88ef2aff2b4fa329ce00a6
Arseny Shishaev <ashishae@student.42.fr>
2020-03-27 22:50:14 +0100

    Add influxdb&grafana and add telegraph stats

--- mysql/srcs/start.sh
+++ mysql/srcs/start.sh
@@ -1,7 +1,11 @@
-MYSQL_ROOT_PASSWORD=`1234`
-MYSQL_PASSWORD=`1234`
+MYSQL_ROOT_PASSWORD=1234
+MYSQL_PASSWORD=1234
+PMA=1234
 echo "root: ${MYSQL_ROOT_PASSWORD}"
 echo "wpusr: ${MYSQL_PASSWORD}"
+echo "pmausr: ${MYSQL_PASSWORD}"
+
+
 
 cat << EOF > /init.sql
 USE mysql;
@@ -10,10 +14,13 @@ GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT O
 GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
 SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 CREATE DATABASE IF NOT EXISTS wpdb;
-GRANT ALL ON wpdb.* to 'wpusr'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE USER 'wpusr'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL ON wpdb.* to 'wpusr'@'%';
+CREATE USER 'pmausr'@'%' IDENTIFIED BY '$PMA_PASSWORD';
+GRANT ALL ON *.* TO 'pmausr'@'%'
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 
 EOF
 
-/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < ./init.sql ; /usr/bin/mysqld --user=mysql --verbose=0 --skip-name-resolve --skip-networking=0 < ./init.sql
+/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < ./init.sql ; /usr/bin/mysqld --user=mysql --skip-name-resolve --skip-networking=0
--- start.sh
+++ start.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-# Start minikube
-minikube start --vm-driver=virtualbox
+# Start minikube 
+minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=3000-32767
 minikube addons enable ingress
 
 CLUSTER_IP="$(minikube ip)"
 sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" ftps/srcs/vsftpd.conf
-# Set Docker links to Minikube Docker
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" wordpress/srcs/wp-config.php
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" phpmyadmin/srcs/config.inc.php
 
-eval $(minikube docker-env) ; docker build ./nginx/ --tag angi ; docker build ./ftps/ --tag ftps; docker build ./mysql --tag mysql
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" grafana/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" influxdb/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" mysql/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" nginx/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" phpmyadmin/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" wordpress/srcs/telegraf.conf
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" ftps/srcs/telegraf.conf
+
+sed -i "" "s/CLUSTER_IP/$CLUSTER_IP/g" grafana/srcs/provisioning/datasources/all.yaml
+
+
+# Set Docker links to Minikube Docker and build all images
+
+eval $(minikube docker-env) ; docker build ./nginx/ --tag angi ;\
+docker build ./ftps/ --tag ftps; \
+docker build ./mysql --tag mysql; \
+docker build ./wordpress --tag wordpress; \
+docker build ./phpmyadmin --tag phpmyadmin;\
+docker build ./influxdb --tag influxdb;\
+docker build ./grafana --tag grafana;\
+
 sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" ftps/srcs/vsftpd.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" wordpress/srcs/wp-config.php
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" phpmyadmin/srcs/config.inc.php
+
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" grafana/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" influxdb/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" mysql/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" nginx/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" phpmyadmin/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" wordpress/srcs/telegraf.conf
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" ftps/srcs/telegraf.conf
+
+
+
+sed -i "" "s/$CLUSTER_IP/CLUSTER_IP/g" grafana/srcs/provisioning/datasources/all.yaml
+
 
 kubectl apply -f jinx.yaml
 
